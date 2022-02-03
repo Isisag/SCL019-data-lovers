449d5f4474a772faa7fe608eb538fc22a46bbc08
andreasvidal <andrea.s.vidal00@gmail.com>
2022-02-03 18:03:14 -0400

    arreglo en archivos js

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ console.log(data)
 export const results = data.results; //array
 
 // devuelve el valor de origen y genero de los personajes
-//(({ origin }) se estrae el valor del elemento en el array
+//(({ origin }) se extrae el valor del elemento en el array
 const origin = results.map(({ origin }) => origin);
 const gender = results.map(({ gender }) => gender)
 console.log(gender);
@@ -27,44 +27,6 @@ console.log(origin);
 //function filterSpecies(data, ordenamiento)   => comprar en click la condicion
 // export function filterSpecies(){
 
-<<<<<<< HEAD
-export function filterSpecies() {
-
-    let aliensFilter = results.filter((elements) => elements.species == "Alien" && elements.species == "Human" && elements.species == "Humanoid");
-
-    console.log(aliensFilter);
-
-
-}
-filterSpecies();
-
-export const SortfilterAz = (data) => {
-    const sortAz = data.sort((a, b) => {
-        const name1 = a.name.toLowerCase()
-        const name2 = b.name.toLowerCase()
-        if (name1 > name2) return -1;
-        else return 1;
-        //  return a.title.localCompare(b.title);
-    });
-    console.log("ESTE ES AZ")
-    return sortAz;
-}
-export const SortfilterZa = (data) => {
-    const sortZa = data.sort((a, b) => {
-        const name1 = a.name.toLowerCase()
-        const name2 = b.name.toLowerCase()
-        if (name1 < name2) return 1;
-        else return -1;
-
-        //  return b.title.localCompare(a.title);
-    });
-
-    return sortZa;
-}
-
-console.log(SortfilterAz(data.results));
-console.log(SortfilterZa(data.results))
-=======
 //   let aliensFilter = results.filter((elements)=> elements.species == "Alien" );
 //   return aliensFilter;
 
@@ -89,7 +51,6 @@ export function filterRobot() {
     let robotFilter = results.filter((elements) => elements.species == "Robot");
     return robotFilter
 }
->>>>>>> bb81126844c23e4b7f56528fdb90e427d2d324b1
 
 export function filterSpecies() {
     return "hola"
@@ -124,48 +85,43 @@ export function filterSpecies() {
 //     return sortZa;
 //   }
 
-<<<<<<< HEAD
-// FUNCION QUE RECORRE LA DATA Y DEVUELVE SUS VALORES SOLO EN CONSOLE
-function getDataElements() {
-=======
 //   console.log(SortfilterAz(data.results));
 //   console.log(SortfilterZa(data.results))
->>>>>>> bb81126844c23e4b7f56528fdb90e427d2d324b1
 
-    let elementos = results.map.(function(item) {
+let elementos = results.map.(function(item) {
 
-<<<<<<< HEAD
-        // resultados planos (es decir adentro de estos no hay otro array)
-        const charactersName = item.name
-        const charactersSpecies = item.species
-        const charactersImage = item.image
-        const charactersStatus = item.status
 
-        // resultados dentro de arrays (hay que llamar a la propiedad externa primero antes de acceder a ellos)
-        const charactersLocation = item.location
-        const charactersLocationName = charactersLocation.name
-        const charactersLocationUrl = charactersLocation.url
+    // resultados planos (es decir adentro de estos no hay otro array)
+    const charactersName = item.name
+    const charactersSpecies = item.species
+    const charactersImage = item.image
+    const charactersStatus = item.status
 
-        const charactersOrigin = item.origin
-        const charactersOriginName = charactersOrigin.name
-        const charactersOriginUrl = charactersOrigin.url
+    // resultados dentro de arrays (hay que llamar a la propiedad externa primero antes de acceder a ellos)
+    const charactersLocation = item.location
+    const charactersLocationName = charactersLocation.name
+    const charactersLocationUrl = charactersLocation.url
 
-        const charactersEpisodes = item.episode
+    const charactersOrigin = item.origin
+    const charactersOriginName = charactersOrigin.name
+    const charactersOriginUrl = charactersOrigin.url
 
-        // console.log(charactersName)
+    const charactersEpisodes = item.episode
 
-        // return {
-        //     charactersName,
-        //     charactersSpecies,
-        //     charactersImage,
-        //     charactersStatus
-        // }
+    // console.log(charactersName)
 
-    })
+    // return {
+    //     charactersName,
+    //     charactersSpecies,
+    //     charactersImage,
+    //     charactersStatus
+    // }
 
-}
+})
+
+
 getDataElements();
-=======
+
 // FUNCION QUE RECORRE LA DATA Y DEVUELVE SUS VALORES SOLO EN CONSOLE
 // function getDataElements(){
 
@@ -247,8 +203,34 @@ console.log(myArr)
 function unico(arr) {
     return Array.from(new Set(arr))
 }
-<<<<<<< HEAD
-=======
+// }getDataElements();
 
->>>>>>> bb81126844c23e4b7f56528fdb90e427d2d324b1
->>>>>>> 51f09f4d625e870a65462361f6ff2c4281102851
+
+
+// esta función ejecuta la muestra de datos de un personaje de manera estatica
+function showCharacters() {
+
+    const h3 = document.createElement('h3');
+    const textAppend = document.body.appendChild(h3);
+    const characterName = textAppend.innerHTML = `${rickSanchez.name}`
+
+    const imgC = document.createElement("img")
+    const imgAppend = document.body.appendChild(imgC)
+    const characterImage = imgAppend.src = `${rickSanchez.image}`
+
+
+
+}
+
+
+
+let setear = new Set([results])
+    // let setear2 = new Set.keys([results])
+for (let species of setear) console.log(species);
+for (let item of setear.keys()) console.log(item);
+const myArr = Array.from(setear)
+console.log(myArr)
+
+function unico(arr) {
+    return Array.from(new Set(arr))
+}
--- src/main.js
+++ src/main.js
@@ -21,8 +21,6 @@ console.log(example, data);
 //     const textAppend = document.body.appendChild(h3);
 //     const characterName = textAppend.innerHTML = `${rickSanchez.name}`
 
-<<<<<<< HEAD
-=======
 //     const imgC = document.createElement("img")
 //     const imgAppend = document.body.appendChild(imgC)
 //     const characterImage = imgAppend.src = `${rickSanchez.image}`
@@ -30,7 +28,6 @@ console.log(example, data);
 
 // funcion para mostrar en pantalla contenedor con img y name desde la data
 // variable "results" contiene toda la data
->>>>>>> 9b504a00f2f9928d01466ae7ef002a2a64020785
 function showCharacters(results) {
     let container = d.getElementById("container")
 
@@ -53,29 +50,14 @@ function showCharacters(results) {
         status.setAttribute("id", characterStatus)
 
         name.innerHTML = charactersName
-<<<<<<< HEAD
-        status.innerHTML = characterStatus
-
-        // const nameCard = name.innerHTML = `${charactersName}`
-
-
-=======
             // const nameCard = name.innerHTML = `${charactersName}`
->>>>>>> 9b504a00f2f9928d01466ae7ef002a2a64020785
 
         let prueba = container.appendChild(div);
         let prueba2 = div.appendChild(image)
         let prueba3 = div.appendChild(name)
         let prueba4 = div.appendChild(status)
 
-        return {
-
-<<<<<<< HEAD
-=======
-        return {
-
->>>>>>> 9b504a00f2f9928d01466ae7ef002a2a64020785
-        }
+        return {}
 
     })
 }
@@ -98,89 +80,13 @@ function menuFilter() {
 
 
 
-    // function menuFilter(){
+    /*// function menuFilter(){
 
     // const speciesOptions1 = d.getElementById("species_options1") //human
     // const speciesOptions2 = d.getElementById("species_options2") // alien
     // const speciesOptions3 = d.getElementById("species_options3") // robot
     // const speciesOptions4 = d.getElementById("species_options4") // humanoide
 
-<<<<<<< HEAD
-// speciesOptions1.addEventListener("click", () => {
-//     filterHuman();
-
-//     const h3 = document.createElement('h3');
-//     const textAppend = document.body.appendChild(h3);
-//     const characterName = textAppend.innerHTML = `${filterHuman()}`
-
-//     const results = data.results;
-//     const species = results
-//     // console.log(filterHuman())
-// })
-
-// speciesOptions2.addEventListener("click", () => {
-//     console.log(filterAlien())
-// })
-
-// speciesOptions3.addEventListener("click", () => {
-//     console.log(filterRobot())
-// })
-
-
-
-// }menuFilter()
-
-
-
-// FUNCION QUE RECORRE LA DATA Y DEVUELVE SUS VALORES SOLO EN CONSOLE
-// function getDataElements(){
-
-//     let elementos = results.map(function(item) {
-
-//       // resultados planos (es decir adentro de estos no hay otro array)
-//       const charactersName = item.name
-//       const charactersSpecies = item.species
-//       const charactersImage = item.image
-//       const charactersStatus = item.status
-
-//       // resultados dentro de arrays (hay que llamar a la propiedad externa primero antes de acceder a ellos)
-//       const charactersLocation = item.location
-//       const charactersLocationName = charactersLocation.name
-//       const charactersLocationUrl = charactersLocation.url
-
-//       const charactersOrigin = item.origin
-//       const charactersOriginName = charactersOrigin.name
-//       const charactersOriginUrl = charactersOrigin.url
-
-//       const charactersEpisodes = item.episode
-
-//         //
-
-//       const h3 = document.createElement('h3');
-//       const textAppend = document.body.appendChild(h3);
-//       const characterName = textAppend.innerHTML = `${charactersName}`
-
-//       const imgC = document.createElement("img")
-//       const imgAppend = document.body.appendChild(imgC)
-//       const characterImage = imgAppend.src = `${charactersImage}`
-
-//       return {
-//         characterName, characterImage
-//       }
-
-//       // console.log(charactersName)
-
-//       // return {
-//       //     charactersName,
-//       //     charactersSpecies,
-//       //     charactersImage,
-//       //     charactersStatus
-//       // }
-
-//     })
-
-//   }getDataElements();
-=======
     // speciesOptions1.addEventListener("click", () => {
     //     filterHuman();
 
@@ -255,4 +161,80 @@ function menuFilter() {
     //     })
 
     //   }getDataElements();
->>>>>>> 9b504a00f2f9928d01466ae7ef002a2a64020785
+    // speciesOptions1.addEventListener("click", () => {
+    //     filterHuman();
+
+    //     const h3 = document.createElement('h3');
+    //     const textAppend = document.body.appendChild(h3);
+    //     const characterName = textAppend.innerHTML = `${filterHuman()}`
+
+    //     const results = data.results;
+    //     const species = results
+    //     // console.log(filterHuman())
+    // })
+
+    // speciesOptions2.addEventListener("click", () => {
+    //     console.log(filterAlien())
+    // })
+
+    // speciesOptions3.addEventListener("click", () => {
+    //     console.log(filterRobot())
+    // })
+
+
+
+    // }menuFilter()
+
+
+
+    // FUNCION QUE RECORRE LA DATA Y DEVUELVE SUS VALORES SOLO EN CONSOLE
+    // function getDataElements(){
+
+    //     let elementos = results.map(function(item) {
+
+    //       // resultados planos (es decir adentro de estos no hay otro array)
+    //       const charactersName = item.name
+    //       const charactersSpecies = item.species
+    //       const charactersImage = item.image
+    //       const charactersStatus = item.status
+
+    //       // resultados dentro de arrays (hay que llamar a la propiedad externa primero antes de acceder a ellos)
+    //       const charactersLocation = item.location
+    //       const charactersLocationName = charactersLocation.name
+    //       const charactersLocationUrl = charactersLocation.url
+
+    //       const charactersOrigin = item.origin
+    //       const charactersOriginName = charactersOrigin.name
+    //       const charactersOriginUrl = charactersOrigin.url
+
+    //       const charactersEpisodes = item.episode
+
+    //         //
+
+    //       const h3 = document.createElement('h3');
+    //       const textAppend = document.body.appendChild(h3);
+    //       const characterName = textAppend.innerHTML = `${charactersName}`
+
+    //       const imgC = document.createElement("img")
+    //       const imgAppend = document.body.appendChild(imgC)
+    //       const characterImage = imgAppend.src = `${charactersImage}`
+
+    //       return {
+    //         characterName, characterImage
+    //       }
+
+    //       // console.log(charactersName)
+
+    //       // return {
+    //       //     charactersName,
+    //       //     charactersSpecies,
+    //       //     charactersImage,
+    //       //     charactersStatus
+    //       // }
+
+    //     })
+
+    //   }getDataElements();
+     }
+
+*/
